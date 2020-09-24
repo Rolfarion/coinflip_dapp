23090787d207d393180d42a2bd3feba737b728c8
Rolf Veringmeier <rolfveringmeier@hotmail.com>
2020-09-24 17:09:55 +0200

    Update the Abi file

--- js/abi.js
+++ js/abi.js
@@ -1,194 +1,248 @@
 var abi = [
-   {
-     "anonymous": false,
-     "inputs": [
-       {
-         "indexed": false,
-         "internalType": "bytes32",
-         "name": "queryID",
-         "type": "bytes32"
-       },
-       {
-         "indexed": false,
-         "internalType": "uint256",
-         "name": "playerChoice",
-         "type": "uint256"
-       },
-       {
-         "indexed": false,
-         "internalType": "address",
-         "name": "playerAddress",
-         "type": "address"
-       }
-     ],
-     "name": "flipRes",
-     "type": "event"
-   },
-   {
-     "anonymous": false,
-     "inputs": [
-       {
-         "indexed": false,
-         "internalType": "uint256",
-         "name": "randomNumber",
-         "type": "uint256"
-       }
-     ],
-     "name": "generatedRandomNumber",
-     "type": "event"
-   },
-   {
-     "anonymous": false,
-     "inputs": [
-       {
-         "indexed": false,
-         "internalType": "uint256",
-         "name": "value",
-         "type": "uint256"
-       },
-       {
-         "indexed": false,
-         "internalType": "address",
-         "name": "player",
-         "type": "address"
-       }
-     ],
-     "name": "invest",
-     "type": "event"
-   },
-   {
-     "constant": true,
-     "inputs": [
-       {
-         "internalType": "bytes32",
-         "name": "",
-         "type": "bytes32"
-       }
-     ],
-     "name": "currentBets",
-     "outputs": [
-       {
-         "internalType": "address payable",
-         "name": "playerAddress",
-         "type": "address"
-       },
-       {
-         "internalType": "bytes32",
-         "name": "betID",
-         "type": "bytes32"
-       },
-       {
-         "internalType": "uint256",
-         "name": "playerChoice",
-         "type": "uint256"
-       },
-       {
-         "internalType": "uint256",
-         "name": "betAmount",
-         "type": "uint256"
-       }
-     ],
-     "payable": false,
-     "stateMutability": "view",
-     "type": "function"
-   },
-   {
-     "constant": true,
-     "inputs": [],
-     "name": "latestNumber",
-     "outputs": [
-       {
-         "internalType": "uint256",
-         "name": "",
-         "type": "uint256"
-       }
-     ],
-     "payable": false,
-     "stateMutability": "view",
-     "type": "function"
-   },
-   {
-     "constant": false,
-     "inputs": [
-       {
-         "internalType": "bytes32",
-         "name": "_myid",
-         "type": "bytes32"
-       },
-       {
-         "internalType": "string",
-         "name": "_result",
-         "type": "string"
-       }
-     ],
-     "name": "__callback",
-     "outputs": [],
-     "payable": false,
-     "stateMutability": "nonpayable",
-     "type": "function"
-   },
-   {
-     "constant": false,
-     "inputs": [
-       {
-         "internalType": "bytes32",
-         "name": "_queryID",
-         "type": "bytes32"
-       },
-       {
-         "internalType": "string",
-         "name": "_result",
-         "type": "string"
-       },
-       {
-         "internalType": "bytes",
-         "name": "_proof",
-         "type": "bytes"
-       }
-     ],
-     "name": "__callback",
-     "outputs": [],
-     "payable": false,
-     "stateMutability": "nonpayable",
-     "type": "function"
-   },
-   {
-     "constant": false,
-     "inputs": [
-       {
-         "internalType": "uint256",
-         "name": "playerChoice",
-         "type": "uint256"
-       }
-     ],
-     "name": "flipCoin",
-     "outputs": [],
-     "payable": true,
-     "stateMutability": "payable",
-     "type": "function"
-   },
-   {
-     "constant": false,
-     "inputs": [
-       {
-         "internalType": "uint256",
-         "name": "value",
-         "type": "uint256"
-       }
-     ],
-     "name": "investToContract",
-     "outputs": [],
-     "payable": true,
-     "stateMutability": "payable",
-     "type": "function"
-   },
-   {
-     "constant": false,
-     "inputs": [],
-     "name": "payOut",
-     "outputs": [],
-     "payable": true,
-     "stateMutability": "payable",
-     "type": "function"
-   }
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "message",
+        "type": "string"
+      }
+    ],
+    "name": "flipMessage",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "bytes32",
+        "name": "queryID",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "playerChoice",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "playerAddress",
+        "type": "address"
+      }
+    ],
+    "name": "flipRes",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "randomNumber",
+        "type": "uint256"
+      }
+    ],
+    "name": "generatedRandomNumber",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "player",
+        "type": "address"
+      }
+    ],
+    "name": "invest",
+    "type": "event"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "name": "currentBets",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "playerAddress",
+        "type": "address"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "betID",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "uint256",
+        "name": "playerChoice",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "betAmount",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "latestNumber",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "_myid",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "string",
+        "name": "_result",
+        "type": "string"
+      }
+    ],
+    "name": "__callback",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "_queryID",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "string",
+        "name": "_result",
+        "type": "string"
+      },
+      {
+        "internalType": "bytes",
+        "name": "_proof",
+        "type": "bytes"
+      }
+    ],
+    "name": "__callback",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "playerChoice",
+        "type": "uint256"
+      }
+    ],
+    "name": "flipCoin",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "bool",
+        "name": "value",
+        "type": "bool"
+      },
+      {
+        "internalType": "address",
+        "name": "adr",
+        "type": "address"
+      }
+    ],
+    "name": "setAwaitBets",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "adr",
+        "type": "address"
+      }
+    ],
+    "name": "getAwaitBets",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "investToContract",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "payOut",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  }
 ]
